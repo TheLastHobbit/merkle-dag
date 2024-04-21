14326e288c2a0d2d047c2ac8acf8b735979d6b06
TheLastHobbit <2865755738@qq.com>
2024-04-21 21:09:42 +0800

    update test

--- dag.go
+++ dag.go
@@ -1,19 +1,89 @@
 package merkledag
 
-import "hash"
+import (
+    "encoding/json"
+    "errors"
+    "hash"
+    "strconv"
+)
+
+const (
+    K         = 1 << 10
+    FILE_SIZE = 256 * K
+)
 
 type Link struct {
-	Name string
-	Hash []byte
-	Size int
+    Name string
+    Hash []byte
+    Size int
 }
 
 type Object struct {
-	Links []Link
-	Data  []byte
+    Links []Link
+    Data  []byte
 }
 
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+func Add(store KVStore, node Node, h hash.Hash) ([]byte, error) {
+    switch node.Type() {
+    case FILE:
+        return handleFileNode(store, node.(File), h)
+    case DIR:
+        return handleDirNode(store, node.(Dir), h)
+    default:
+        return nil, errors.New("unsupported node type")
+    }
+}
+
+func handleFileNode(store KVStore, fileNode File, h hash.Hash) ([]byte, error) {
+    fileData := fileNode.Bytes()
+    fileSize := len(fileData)
+    if fileSize > FILE_SIZE {
+        return handleLargeFile(store, fileData, fileSize, h)
+    }
+    return storeObject(store, nil, fileData, h)
+}
+
+func handleLargeFile(store KVStore, data []byte, size int, h hash.Hash) ([]byte, error) {
+    list := Object{Data: []byte("list")}
+    for i := 0; i*size/FILE_SIZE < len(data); i++ {
+        start := i * FILE_SIZE
+        end := start + FILE_SIZE
+        if end > len(data) {
+            end = len(data)
+        }
+        partData := data[start:end]
+        hash, err := storeObject(store, nil, partData, h)
+        if err != nil {
+            return nil, err
+        }
+        list.Links = append(list.Links, Link{Name: "part" + strconv.Itoa(i), Hash: hash, Size: end - start})
+    }
+    return storeObject(store, list.Links, list.Data, h)
+}
+
+func handleDirNode(store KVStore, dirNode Dir, h hash.Hash) ([]byte, error) {
+    tree := Object{Data: []byte("tree")}
+    it := dirNode.It()
+    for it.Next() {
+        child := it.Node()
+        childHash, err := Add(store, child, h)
+        if err != nil {
+            return nil, err
+        }
+        tree.Links = append(tree.Links, Link{Name: child.Name(), Hash: childHash, Size: len(child.Bytes())})
+    }
+    return storeObject(store, tree.Links, tree.Data, h)
+}
+
+func storeObject(store KVStore, links []Link, data []byte, h hash.Hash) ([]byte, error) {
+    obj := Object{Links: links, Data: data}
+    objBytes, err := json.Marshal(obj)
+    if err != nil {
+        return nil, err
+    }
+    h.Reset()
+    h.Write(objBytes)
+    hash := h.Sum(nil)
+    store.Put(hash, objBytes)
+    return hash, nil
 }
--- dag2file.go
+++ dag2file.go
@@ -3,5 +3,44 @@ package merkledag
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
 	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
+	obj, success := store.Get(hash)
+	if success != nil {
+		panic("Hash2File: hash not found")
+	}
+
+	// 获取了data的二进制文件
+	// 转化为对象
+	data, err := Decode(obj)
+
+	if data.Links == nil {
+	    //link为空，说明为文件
+		return data.Data
+	}
+	else{
+		// links不为空，则说明还是还不是叶子节点
+		// 递归调用
+		headPath, tailPath := splitPath(path)
+		for _, link := range data.Links {
+		    if  link.Name == headPath{
+			// 递归调用
+			Hash2File(store, link.Hash, path, hp)
+			}
+		}
+
+	}
+
 	return nil
+}
+
+//处理string类型的path
+func splitPath(path string) (head, tail string) {
+	parts := strings.Split(path, "/")
+	if len(parts) > 1 {
+		head = parts[0]
+		tail = strings.Join(parts[1:], "/")
+	} else {
+		head = parts[0]
+		tail = ""
+	}
+	return
 }
